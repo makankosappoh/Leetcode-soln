3d01a35d2a3a4b13e53cb4c40e63e28457a785c7
Saharsh <139987384+makankosappoh@users.noreply.github.com>
2025-05-20 16:59:12 +0530

    Time: 8 ms (8.04%), Space: 28.2 MB (24.16%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        return nums[n/2];
+    }
+};
+//this sorting method is not reliable as its only returning n/2th index.......come back and try with other algos and other method like hashmap, bv algo etc
