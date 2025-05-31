9f48a5c617d564721af575e04b0c47b1840547a4
Saharsh <139987384+makankosappoh@users.noreply.github.com>
2025-05-31 13:39:20 +0530

    TWO METHODS BUT FIRST ONE HAS BEST TC/SC
    there are two methods, 
    The first one is the ideal that Leetcode Q is asking with O(1) and O(N) 
    
    The second one is easiest, a 3-4 line code, but does not have the best TC/SC as O(1)  and O(NLOGN)

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -23,3 +23,26 @@ public:
     return n + 1;
     }
 };
+
+
+/*
+SECOND EASY SOLUTION BUT TIME/SPACE COMPLX IS NOT GOOD 
+class Solution {
+public:
+// sort the array eg: [1, 3, 4], with set target = 1
+// (n>0) n start with 1 also n == target, so target is incremented to 2.
+// next iteration n = 3: n is greater than target (2), so returns 2 as the smallest missing positive integer.
+    int firstMissingPositive(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int target = 1;
+        for (int n : nums) {
+            if (n > 0 && n == target) {
+                target++;
+            } else if (n > target) {
+                return target;
+            }
+        }
+        return target;
+    }
+};
+*/
