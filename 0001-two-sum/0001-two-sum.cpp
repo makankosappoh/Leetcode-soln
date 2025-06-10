95b041210f460ae6f89f61ef84ea2a1daf28159b
Saharsh <139987384+makankosappoh@users.noreply.github.com>
2025-06-10 20:27:03 +0530

    Update 0001-two-sum.cpp

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         for(int i = 0; i < nums.size();i++){//iterate each element for idx i
-            for(int j = i + 1; j < nums.size(); j++){//also iterate idx j from i + 1 next element
+            for(int j = i + 1; j < nums.size(); j++){//also iterate idx j from i + 1 next element to i 
                 if(nums[i]+nums[j] == target){
                     return {i,j};//we use another j bcz we need two seperate indexes
                 }
